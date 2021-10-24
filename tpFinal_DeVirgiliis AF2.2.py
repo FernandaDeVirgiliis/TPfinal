48a2f61bc4c858db45f3ea6297d0ce3cf1a5aac0
FernandaDeVirgiliis <devirgiliisfernanda@gmail.com>
2021-10-24 19:04:29 -0300

    desarrollo interfaz grafica

--- tpFinal_DeVirgiliis AF2.2.py
+++ tpFinal_DeVirgiliis AF2.2.py
@@ -6,3 +6,15 @@ from tkinter import messagebox
 import sqlite3
 
 
+root=T()
+root.tittle("aplicacion CRUD CON B.D)
+root.geometry("500x350")
+
+miLegajo=StringVar()
+miNombre=StringVar()
+miCargo=StringVar()
+miSucursal=StringVar()
+
+def conexionBBDD():
+    miConexion=sqlite3.connect("bd")
+    miCursor=miConexion.cursor()
