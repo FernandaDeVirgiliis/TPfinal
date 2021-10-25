13059052b5c4448cadac880c60bca850040b4c60
FernandaDeVirgiliis <devirgiliisfernanda@gmail.com>
2021-10-25 20:54:41 -0300

    creacion de tablas

--- tpFinal_DeVirgiliis AF2.2.py
+++ tpFinal_DeVirgiliis AF2.2.py
@@ -1,14 +1,13 @@
-import sqlite3
 
-con = sqlite3.connect('mydatabase.db')
 from tkinter import *
 from tkinter import messagebox
+from tkinter import ttk
 import sqlite3
 
 
-root=T()
-root.tittle("aplicacion CRUD CON B.D)
-root.geometry("500x350")
+root=Tk()
+root.title("Programa CRUD con B.D")
+root.geometry("600x350")
 
 miLegajo=StringVar()
 miNombre=StringVar()
@@ -18,3 +17,47 @@ miSucursal=StringVar()
 def conexionBBDD():
     miConexion=sqlite3.connect("bd")
     miCursor=miConexion.cursor()
+
+#Creación de tablas
+    
+    try:
+        miCursor.excute
+        ('''
+            CREATE TABLE empleado (
+            LEGAJO INTEGER PRIMARY KEY AUTOINCREMENT,
+            NOMBRE VARCHAR(50),
+            CARGO VARCHAR(50),
+            SUCURSAL VARCHAR(50)
+        ''')
+        messagebox.showinfo("CONEXION","Base de datos creada exitosamente")
+    except:
+        ""
+
+#Eliminar BD
+        
+def eliminarBBDD():
+    miConexion=sqlite3.connect("bd")
+    miCursor=miConexion.cursor()
+    if messagebox.askyesno(message="Los datos seran eliminados ¿Desea continuar?", tittle="ADVERTENCIA"):
+        miCursor.execute("DROPE TABLE empleado")
+    else:
+        pass
+
+
+#Salir del programa
+
+def salirPrograma():
+    valor=messagebox.askquestion("Salir","¿Desea salir?")
+    if valor=="yes":
+        root.destroy()
+
+#Limpiar campos
+
+def limpiarCampos():
+    miLegajo.set("")
+    miNombre.set("")
+    miCargo.set("")
+    miSucursal.set("")
+
+
+
